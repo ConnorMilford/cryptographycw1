00a542c3ce0da945bb0c37946535bada58cefd92
Connor <connormilford@hotmail.com>
2025-02-26 17:32:06 +0000

    Finished functions runs very slowly.

--- problem6.py
+++ problem6.py
@@ -1,43 +1,57 @@
+import math
+
 class Problem6:
+
 
     def __init__(self):
         self.params = {}
+    
         #Place holder till key is found
-        self.key = 0 
-
-
+        self.key = 0
+        
+    
     def printParams(self):
         print(self.params)
+        
     
-
     def openFile(self, file):
         with open(file, "r") as f:
             for line in f:
                 line = line.strip()
                 if not line:
                     continue
-                
-                key, value = line.strip().split("=")
+                key, value = line.split("=")
                 key = key.strip()
                 value = value.strip()
-
-                self.params[key] = value 
-
+                self.params[key] = value
+                
     #m1 size 2^19 < m1 < 2^20
     #Allows brute force
     def bruteForceMessage(self):
+
         minSize = pow(2, 19)
         maxSize = pow(2, 20)
-        
 
-        for i in range(minSize, maxSize):    
+        for i in range(minSize, maxSize):
+            print(f"Checked {i} keys out of {maxSize}".format(i, maxSize))
             key = self.computeKey(i)
-            
-            if self.checkM2Candidate(self.decryptC2WithKey(key)):
+            if key is None:
+                continue
+
+            m2_int = self.decryptC2WithKey(key)
+
+            if m2_int is None:
+                continue
+
+            m2String = self.intToString(m2_int)
+
+            if self.checkM2Candidate(m2String):
+                print("Found key: ", key)
                 self.key = key
-            
-
-
+                print("Decrypted Message: ", m2String)
+                break
+                
+    
     #c1-ciphertext
     #p - prime
     # Key = c1 * (m1)^-1 % p
@@ -45,26 +59,33 @@ class Problem6:
         cipherText = int(self.params['c1'])
         prime = int(self.params['p'])
         #Inverse modulo of m1
+        if math.gcd(m1, prime) != 1:
+            return None
         inverseModM1 = pow(m1, -1, prime)
-
         return (cipherText * inverseModM1) % prime
-        
         
     
     def decryptC2WithKey(self, key):
         cipherText2 = int(self.params['c2'])
         prime = int(self.params['p'])
+        if math.gcd(key, prime) != 1:
+            return None
         inverseModKey = pow(key, -1, prime)
-
         return (cipherText2 * inverseModKey) % prime
-
+        
     
     # returns boolean based on whether the string contains " THE " & " TO "
     def checkM2Candidate(self, string):
-        if not isinstance(string, str):
-            return False
+        return isinstance(string, str) and " THE " in string.upper() and " TO " in string.upper()
         
-        if (string.find(" THE ") + string.find(" TO ") > 0):
-            return True
-        else: 
-            return False
+    
+    def intToString(self, m2):
+        try:
+            hex_string = hex(m2)[2:] # Convert to hex string
+            # Add a leading zero if the hex string length is odd
+            if len(hex_string) % 2 != 0:
+                hex_string = '0' + hex_string
+            return bytes.fromhex(hex_string).decode('utf-8', errors='ignore')
+        except:
+            
+            return None
